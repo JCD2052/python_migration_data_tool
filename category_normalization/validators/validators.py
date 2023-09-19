3bc68a3b8708389d3ce5c50121af00c8e7e71918
v.peresunko <v.peresunko@a1qa.com>
2023-09-19 10:47:07 +0300

    small changes.

--- category_normalization/validators/validators.py
+++ category_normalization/validators/validators.py
@@ -94,10 +94,10 @@ class AlmostSameWordValidator(BaseValidator):
                 f'{" ,".join(self.__duplicate_dict.get(self.__normalize_category(value)))}')
 
     @staticmethod
-    def __get_duplicate_values_with_normalization(values_list: list) -> Set[str]:
+    def __get_duplicate_values_with_normalization(values_list: list) -> dict:
         values_list = list(set([value.replace(" And ", " and ").replace(SPACE_STRING, '') for value in values_list]))
         values = {v: AlmostSameWordValidator.__normalize_category(v) for v in values_list}
-        return BaseValidator.find_duplicates_in_dict(values)
+        return BaseValidator.build_duplicates_dict(values)
 
     @staticmethod
     def __normalize_category(category_string: str) -> str:
